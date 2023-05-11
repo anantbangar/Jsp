287e799527d9aa5ab92c180b31bffe8b921ba8f3
anantbangar <anant.bangar07@gmail.com>
2023-05-11 11:51:16 +0530

    adding all projects

--- JSP16RegistrationForm/src/com/registrationform/Provider.java
+++ JSP16RegistrationForm/src/com/registrationform/Provider.java
@@ -4,10 +4,10 @@ public interface Provider
 {
 	String DRIVER="com.mysql.cj.jdbc.Driver";
 	
-	String CONNECTION_URL="jdbc:mysql://localhost:3306/jsp";
+	String CONNECTION_URL="jdbc:mysql://localhost:3306/hibernate";
 	
 	String USERNAME="root";
 	
-	String PASSWORD="system@123";
+	String PASSWORD="system123";
 	
 }
